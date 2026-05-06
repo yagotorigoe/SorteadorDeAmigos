7c12f4da8664a40b3b2e6bf3cb12ffe4068a0402
Yago Torigoe <japafivem@gmail.com>
2026-05-06 12:03:14 -0300

    Adicionando input

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-let amigos = ["yago", "pedro", "joao", "douglas", "rodolfo"];
+let amigos = [];
+
+function adicionar () {
+   let input = document.getElementById("nome-amigo");
+   let nome = input.value
+
+   if (nome === "") {
+      alert("Por favor, digite um nome!");
+      return
+   }
+
+   amigos.push(nome);
+
+   input.value = "";
+}
 
 function sortear() {
-   let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+   if(amigos.length === 0){
+      alert("Adionar amigos antes de sortear!");
+      return;
+   }
 
+   let numeroAleatorio = Math.floor(Math.random() * amigos.length);
    let nomeSorteado = amigos[numeroAleatorio];
 
    let resultado = document.getElementById("resultado");
